63d79e5ab788150f7d3251144d6d65b3dc6668c0
Jenney Grover <jenneygrover@fullsail.edu>
2013-08-07 23:41:02 -0400

    added jQueryMobile
    added code for main page and navbar pages

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@
 //ASD 1308
 //Buddy Trainer
 //August 2013
+
+$("#homepage footer a").on("click", function() {
+	//when the user clicks the anchor-elements
+	//within the footer-element of the #products iD
+	//do something...
+});
